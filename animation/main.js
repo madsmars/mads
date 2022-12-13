1ea5efcccc9a1e593a47424307f6845f18691ac4
madsmars <mtheli@saic.edu>
2022-12-12 19:48:58 -0600

    debugged some shit.

--- animation/main.js
+++ animation/main.js
@@ -1,9 +1,9 @@
-function myFunction() {
+function toggleDarkMode() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
 
-function myFunction() {
+function expandReadMore() {
     var dots = document.getElementById("dots");
     var moreText = document.getElementById("more");
     var btnText = document.getElementById("myBtn");
@@ -18,3 +18,31 @@ function myFunction() {
       moreText.style.display = "inline";
     }
 }
+
+
+
+displayGalleryOnLoad();
+
+function displayGalleryOnLoad() {
+    
+    const URL = window.location.href.split('#');
+
+    // checks if the default page should be loaded
+    if(URL.length == 1) {
+        return;
+    }
+
+    const selectedGallery = "#" + URL[URL.length - 1];
+    $(`${selectedGallery}`).show();
+    $(".gallery-container").not(`${selectedGallery}`).hide();
+}
+
+// any time one of the .gallery-links is clicked...
+$('.gallery-link').click(function () {
+
+    const selectedGallery = $(this).attr('href');
+    console.log(selectedGallery)
+
+    $(`${selectedGallery}`).show();
+    $(".gallery-container").not(`${selectedGallery}`).hide();
+})
