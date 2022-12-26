2233f2f334cc2d4a904eccbe1fa030a87e6590c8
madsmars <mtheli@saic.edu>
2022-12-26 09:22:25 -0800

    trued making a commit form ?

--- contact/main.js
+++ contact/main.js
@@ -23,3 +23,68 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+form.onsubmit = return ("false") { 
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fields.fname = document.getElementById('fname');
+  fields.subject1 = document.getElementById('subject1');
+  fields.subject = document.getElementById('subject');
+  fields.country = document.getElementById('country');
+  fields.mname = document.getElementById('mname');
+ })
+
+ function isNotEmpty(value) {
+  if (value == null || typeof value == 'undefined' ) return false;
+  return (value.length > 0);
+ }
+
+ function isEmail(subject) {
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regex.test(String(subject).toLowerCase());
+ }
+
+ function fieldValidation(field, validationFunction) {
+  if (field == null) return false;
+ 
+  let isFieldValid = validationFunction(field.value)
+  if (!isFieldValid) {
+  field.className = 'placeholderRed';
+  } else {
+  field.className = '';
+  }
+ 
+  return isFieldValid;
+ }
+
+ function isValid() {
+  var valid = true;
+  
+  valid &= fieldValidation(fields.fname, isNotEmpty);
+  valid &= fieldValidation(fields.subject1, isNotEmpty);
+  valid &= fieldValidation(fields.subject, isNotEmpty);
+  valid &= fieldValidation(fields.country, isNotEmpty);
+  valid &= fieldValidation(fields.mname, isNotEmpty);
+ 
+  return valid;
+ }
+
+ class User {
+  constructor(fname, subject1, subject, country, mname) {
+  this.fname = fname;
+  this.subject1 = subject1;
+  this.subject = subject;
+  this.country = country;
+  this.mname = mname;
+ }
+
+ class User {
+  constructor(fname, subject1, subject, country, mname) {
+  this.fname = fname;
+  this.subject1 = subject1;
+  this.subject = subject;
+  this.country = country;
+  this.mname = mname;
+  }
+ }
