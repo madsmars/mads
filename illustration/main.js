7c53498a1d424a48dcf38b68bdb07a7391ba1886
madsmars <mtheli@saic.edu>
2023-03-30 12:46:58 -0500

    added a font! eek! purty.

--- illustration/main.js
+++ illustration/main.js
@@ -27,3 +27,14 @@ $('.gallery-link').click(function () {
     $(`${selectedGallery}`).show();
     $(".gallery-container").not(`${selectedGallery}`).hide();
 })
+
+function generateAllImages() {
+    const mainImage = $(".gallery a img").parent()
+    const allGallery = $("#all .gallery")
+    
+    for (let i = 0; i < mainImage.length; i++) {
+        allGallery.append(mainImage[i])
+    }
+}
+
+generateAllImages()
